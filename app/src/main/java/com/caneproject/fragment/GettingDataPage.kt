93a1a59e2a6a85ed5d263efe5c4514fdd1c30719
Amin <amin.sakha00@gmail.com>
2022-04-06 13:15:17 +0430

    add date and time to GettingDataPage layout

--- app/src/main/java/com/caneproject/fragment/GettingDataPage.kt
+++ app/src/main/java/com/caneproject/fragment/GettingDataPage.kt
@@ -3,14 +3,17 @@ package com.caneproject.fragment
 import android.app.Activity
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.ali.uneversaldatetools.date.JalaliDateTime
 import com.caneproject.databinding.FragmentGettingDataPageBinding
 import com.caneproject.utils.MakeConnectionToModulo
 import com.otaliastudios.cameraview.CameraView
+import java.text.DateFormat
+import java.util.*
+
 
 var makeConnectionToModulo: MakeConnectionToModulo? = null
 lateinit var camera: CameraView
@@ -35,25 +38,26 @@ class GettingDataPage : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         camera = binding.camera
         camera.setLifecycleOwner(this)
+        val tmp = JalaliDateTime.Now().toString().substring(0, 11)+ "\n"+
+        DateFormat.getDateTimeInstance().format(Date()) .substring(11)
+        binding.dateBox.text = tmp
         if (makeConnectionToModulo?.socket == null || (makeConnectionToModulo?.isBluetoothOn == false)) {
             makeConnectionToModulo =
                 MakeConnectionToModulo(myContext as Activity, myContext)
             makeConnectionToModulo!!.execute()
         }
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
-
 }
 
 fun setTexts(vararg string: String) {
     binding.countBox.text = string[0]
     binding.textView2.text = "${string[1]} , ${string[2]}"
-    val tmp = "${string[3]} , ${string[4]}  ,${string[5]}  ,${string[6]} , ${string[7]} ,${string[8]} "
-    binding.textView3.text = tmp
-
+    binding.textView3.text =
+        "${string[3]} , ${string[4]}  ,${string[5]}  ,${string[6]} , ${string[7]} ,${string[8]} "
 }
